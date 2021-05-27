e1aaf1f70d37c12f4eb972772cc2c16e76bf03ca
Deepa <kosalai17@gmail.com>
2021-05-26 17:07:08 -0700

    page -1

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,18 @@ var body = document.body;
 
 //align Div 
 var quizContainer= document.getElementById("quiz");
+quizContainer.style.width="100vw";
+quizContainer.style.height="100vh";
 quizContainer.display="flex";
-//quizContainer.style.justifyContent ="center";
+quizContainer.style.justifyContent="center";
 quizContainer.style.flexDirection="column";
 quizContainer.style.alignContent="center";
 quizContainer.style.alignItems ="center";
 quizContainer.style.fontFamily="Arial";
-quizContainer.style.justifyContent="center";
 quizContainer.style.textAlign="center";
+quizContainer.style.maxWidth="80rem";
+quizContainer.style.margin ="0 auto";
+
 
 // Add Link to high score 
 var highScoreLink= document.createElement("a");
@@ -19,10 +23,19 @@ highScoreLink.class="btn";
 highScoreLink.style.textAlign="left";
 highScoreLink.style.display="flex";
 highScoreLink.style.alignItems="flex-start";
-highScoreLink.style.marginLeft="50px";
-
-
+highScoreLink.style.flexWrap="wrap";
+highScoreLink.style.marginLeft="50px"
 quizContainer.appendChild(highScoreLink);
+
+var timeDisplayEl =document.createElement("h4");
+timeDisplayEl.textContent="Time : 0";
+timeDisplayEl.style.textAlign="right"
+timeDisplayEl.style.display ="flex";
+
+timeDisplayEl.style.margin="flex-end";
+//timeDisplayEl.style.marginRight="500px";
+
+quizContainer.appendChild(timeDisplayEl);
 
 // Page 1 - Add a centered h1
 var headerElement1 =document.createElement("h1");
@@ -35,11 +48,12 @@ console.log(headerElement1);
 quizContainer.appendChild(headerElement1);
 //body.appendChild(quizContainer);
 
+
 //Add A paragraph 
 var paraElement = document.createElement("p");
 paraElement.style.textAlign="center";
 paraElement.textContent= "Try to answer the following code realted questions within the time limit.Keep in mind that incorrect answers will penalize your score by 10seconds!"
-paraElement.fontSize="38px";
+paraElement.fontSize="large";
 paraElement.style.lineHeight="1.5";
 paraElement.style.wordSpacing="3"
 paraElement.style.marginLeft ="100px";
@@ -60,7 +74,7 @@ buttonElement.style.justifyContent="center";
 buttonElement.style.padding ="12px 28px";
 buttonElement.style.borderRadius="12px";
 buttonElement.onmouseover= function changeColor(){
-this.backgroundColor(green); };
+this.backgroundColor="green"; };
 quizContainer.appendChild(buttonElement);
 
 //Add All the child elements to body 
---
+++ assets/js/script1.js
@@ -0,0 +1,6 @@
+var body = document.body;
+
+var highscoreContainerEl =document.createElement("div");
+highscoreContainerEl.style.display="flex";
+highscoreContainerEl.style.justifyContent="center";
+highscoreContainerEl.style.flexDirection="column";
